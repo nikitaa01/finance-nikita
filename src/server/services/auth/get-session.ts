b16478472b5282fe6c80e860bc8c7f1982ac211d
Nikita Kuzmenko <nikitakuzmenko1107@gmail.com>
2025-01-24 09:22:07 +0100

    remove middleware by redirect from get-session service

--- src/server/services/auth/get-session.ts
+++ src/server/services/auth/get-session.ts
@@ -1,10 +1,15 @@
 import { auth } from "@/server/auth";
 import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export const getSession = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
+
+  if (!session) {
+    redirect("/login");
+  }
 
   return session;
 };
